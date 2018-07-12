bf40269d87cc782cad0568e9a8798cd3fdb9897c
Robert Birkelbach <robert@birkelbach.eu>
2018-07-12 13:47:09 +0200

    Initialize package skelleton

---
+++ R/load_data.R
@@ -0,0 +1,11 @@
+set.seed(1)
+network1 <- matrix(rbinom(n=16*16, size = 1, prob = 0.5), nrow = 16, ncol = 16)
+diag(network1) <- 0
+set.seed(2)
+network2 <- matrix(rbinom(n=16*16, size = 1, prob = 0.5), nrow = 16, ncol = 16)
+diag(network2) <- 0
+nw_list <- list(network1, network2)
+
+networks_dat <- list(waves = length(nw_list),
+                     y = nw_list),
+                     n_actors = nrow(network1))
